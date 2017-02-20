9f89b38fa1928dd6656607196fe6abab0634191b
Anton Karytka <antnkrtk@gmail.com>
2017-02-21 00:45:41 +0300

    Changed some of app's logics. Created local network

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
-var host = "127.0.0.1", port = 33334;
-var dgram = require( "dgram" );
-var client = dgram.createSocket( "udp4" );
-
-client.on( "message", function( msg, rinfo ) {
-    console.log('Current time: ' + msg.toString());
-});
+var host = '192.168.100.8', port = 6001;
+var dgram = require('dgram');
+var client = dgram.createSocket('udp4');
 
 client.bind(port, host);
 
 var message = new Buffer('');
-client.send(message, 0, message.length, 33333, '127.0.0.1');
+client.send(message, 0, message.length, 3000, '192.168.100.4');
+
+client.on('message', function(msg, remote) {
+    console.log('Message recieved from ' + remote.address + ':' + remote.port);
+    console.log('Current time: ' + msg.toString());
+});
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-var host = "127.0.0.1", port = 33333;
-var dgram = require( "dgram" );
-var server = dgram.createSocket( "udp4" );
+var host = '192.168.100.4', port = 3000;
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.bind(port, host);
 
 console.log('UDP server running on ' + host + ':' + port);
 
-server.on( "message", function(msg, remote) {
+server.on('listening', function() {
+    server.setBroadcast(true);
+});
+
+server.on('message', function(msg, remote) {
     console.log('Recieved a message from ' + remote.address + ':' + remote.port + '. Message: ' + msg);
     var date = new Date();
     var currentTime = new Buffer(date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds());
     server.send(currentTime, 0, currentTime.length, remote.port, remote.address);
 });
-
-server.bind(port, host);
