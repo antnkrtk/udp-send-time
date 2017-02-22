af1d431795c1ed4e3f82895ba14bc25351334e99
Anton Karytka <antnkrtk@gmail.com>
2017-02-22 23:26:44 +0300

    Sending requests from client every second. Async working. Optimized output in terminal. Tracking such terminal shortcuts, as ctrl+C.

--- client.js
+++ client.js
@@ -1,13 +1,27 @@
-var host = '192.168.100.8', port = 6001;
+var host = '192.168.100.4', port = 6001;
 var dgram = require('dgram');
 var client = dgram.createSocket('udp4');
 
 client.bind(port, host);
 
-var message = new Buffer('');
-client.send(message, 0, message.length, 3000, '192.168.100.4');
+client.on('listening', function() {
+    sendRequest = setInterval(function() {
+        var msg = new Buffer('');
+        client.send(msg, 0, msg.length, 3000, '192.168.100.8');
+    }, 1000);
+});
 
-client.on('message', function(msg, remote) {
-    console.log('Message recieved from ' + remote.address + ':' + remote.port);
-    console.log('Current time: ' + msg.toString());
+client.on('message', function(msg) {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write('Current time: ' + msg.toString());
+});
+
+process.on('SIGINT', function() {
+    clearInterval(sendRequest);
+    client.close();
+});
+
+client.on('close', function() {
+    console.log('\nConnection closed');
 });
--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-var host = '192.168.100.4', port = 3000;
+var host = '192.168.100.8', port = 3000;
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
 
 server.bind(port, host);
 
-console.log('UDP server running on ' + host + ':' + port);
-
 server.on('listening', function() {
+    console.log('UDP server running on ' + host + ':' + port);
     server.setBroadcast(true);
 });
 
-server.on('message', function(msg, remote) {
-    console.log('Recieved a message from ' + remote.address + ':' + remote.port + '. Message: ' + msg);
+server.on('message', function(msg, rinfo) {
     var date = new Date();
     var currentTime = new Buffer(date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds());
-    server.send(currentTime, 0, currentTime.length, remote.port, remote.address);
+    server.send(currentTime, 0, currentTime.length, rinfo.port, rinfo.address);
 });
