cbc76013e5b9416a8c9760a7c26123f9b5260f3a
Anton Karytka <antnkrtk@gmail.com>
2017-02-23 22:41:25 +0300

    Changed coding style to ES6 convention.

--- client.js
+++ client.js
@@ -1,27 +1,28 @@
-var host = '192.168.100.4', port = 6001;
-var dgram = require('dgram');
-var client = dgram.createSocket('udp4');
+const host = '192.168.100.4';
+const port = 6001;
+const dgram = require('dgram');
+const client = dgram.createSocket('udp4');
 
 client.bind(port, host);
 
-client.on('listening', function() {
-    sendRequest = setInterval(function() {
+client.on('listening', () => {
+    sendRequest = setInterval(() => {
         var msg = new Buffer('');
         client.send(msg, 0, msg.length, 3000, '192.168.100.8');
     }, 1000);
 });
 
-client.on('message', function(msg) {
+client.on('message', (msg) => {
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
-    process.stdout.write('Current time: ' + msg.toString());
+    process.stdout.write(`Current time: ${msg.toString()}`);
 });
 
-process.on('SIGINT', function() {
+process.on('SIGINT', () => {
     clearInterval(sendRequest);
     client.close();
 });
 
-client.on('close', function() {
+client.on('close', () => {
     console.log('\nConnection closed');
 });
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-var host = '192.168.100.8', port = 3000;
-var dgram = require('dgram');
-var server = dgram.createSocket('udp4');
+const host = '192.168.100.8';
+const port = 3000;
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
 
 server.bind(port, host);
 
-server.on('listening', function() {
-    console.log('UDP server running on ' + host + ':' + port);
+server.on('listening', () => {
+    console.log(`UDP server running on ${host}:${port}`);
     server.setBroadcast(true);
 });
 
-server.on('message', function(msg, rinfo) {
+server.on('message', (msg, rinfo) => {
     var date = new Date();
-    var currentTime = new Buffer(date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds());
+    var currentTime = new Buffer(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
     server.send(currentTime, 0, currentTime.length, rinfo.port, rinfo.address);
 });
