329d533ce2d014d847b3185d276c1ba0dd498118
Anton Karytka <antnkrtk@gmail.com>
2017-03-17 12:12:47 +0300

    Minor logics improvement.

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const broadcastAddress = '192.168.100.255';
 server.bind(port, address, () => {
     server.setBroadcast(true);
     console.log(`UDP server running on ${address}:${port}`);
+    
+    setInterval(() => {
+        let date = new Date();
+        let currentTime = new Buffer(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+        server.send(currentTime, port, broadcastAddress);
+    }, 1000);
 });
-
-setInterval(() => {
-    let date = new Date();
-    let currentTime = new Buffer(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
-    server.send(currentTime, port, broadcastAddress);
-}, 1000);
